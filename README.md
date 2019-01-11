d5f4477a9f51fa83331aa68721a8d943be755309
qiaoaltman <1187776733@qq.com>
2019-01-11 14:50:55 +0800

    2019.1.11

---
+++ README.md
@@ -0,0 +1,5 @@
+cnpm install
+
+cnpm run dev
+
+mock.js
---
+++ src/views/nav2/Page5.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="page5">
+		<h1>{{msg}}</h1>
+		<h2>page5</h2>
+		<ul>
+			<li><a href="#" target=""></a></li>
+		</ul>
+		<div class="btn-group" role="group" aria-label="...">
+			<button type="button" class="btn btn-default">left</button>
+			<button type="button" class="btn btn-default">middle</button>
+			<button type="button" class="btn btn-default">right</button>
+		</div>
+		<div id="cc">cc</div>
+	</div>
+</template>
+
+<script type="text/javascript">
+	$(function(){
+		alert(123);
+	});
+	export default{
+		name: 'page5',
+		data(){
+			return{
+				msg: 'xxxxxwelcome'
+			}
+		}
+	}
+</script>
